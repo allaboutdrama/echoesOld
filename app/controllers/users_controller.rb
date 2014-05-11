a6391d1e26414241fc08b7091963f830c036644b
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-05-11 18:37:33 -0400

    Added views for index, new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new 
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save 
+      redirect_to @user
+    else
+      render action: 'new'  
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -19,4 +29,11 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
