dbecd0f1eb2c39a4a6ec8991a4769d08f2519048
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-05-12 17:49:11 -0400

    Added views for the Sounds controller

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -10,14 +10,16 @@ class SoundsController < ApplicationController
   def create
     @sound = Sound.new(user_params)
 
-    if @user.save 
-      redirect_to(:users, notice: 'Your echo has been created')
+    if @sound.save 
+      redirect_to(:sounds, notice: 'Your echo has been created')
     else
-      render action: 'new'  
+      render action: 'new'   
     end
   end
 
   def show
+    @sound = Sound.find(params[:id])
+
   end
 
   def edit
@@ -28,4 +30,10 @@ class SoundsController < ApplicationController
 
   def destroy
   end
+end
+
+
+private
+def user_params
+  params.require(:sound).permit(:name, :description, :latitude, :longitude, :sound_url)
 end
