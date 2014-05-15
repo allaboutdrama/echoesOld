3e38c58747bd321e39601a2980f2a518937ee5e3
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-05-15 13:53:16 -0400

    Added Latitude and Longitude columns to User model

---
+++ db/migrate/20140515174204_add_latitude_and_longitude_to_user.rb
@@ -0,0 +1,6 @@
+class AddLatitudeAndLongitudeToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :latitude, :decimal, precision: 9, scale: 6
+    add_column :users, :longitude, :decimal, precision: 9, scale: 6
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515005648) do
+ActiveRecord::Schema.define(version: 20140515174204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20140515005648) do
     t.datetime "updated_at"
     t.string   "crypted_password"
     t.string   "salt"
+    t.decimal  "latitude",         precision: 9, scale: 6
+    t.decimal  "longitude",        precision: 9, scale: 6
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
